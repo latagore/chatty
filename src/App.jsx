ab53e182841ff7cb516040e662856052da841553
Steven Xu <s@stevenxu.xyz>
2017-06-28 13:42:01 +0000

    add data flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import React, {Component} from 'react';
+import NavBar from './NavBar.jsx';
+import MessageList from './MessageList.jsx'
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id: 0,
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: 1,
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <NavBar />
+        <MessageList messages={this.state.messages}/>
+        <ChatBar currentUser={this.state.currentUser} />
+      </div>
     );
   }
 }
